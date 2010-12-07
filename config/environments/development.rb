f5e0586f7029796ff4727bb49e726ac09a44d880
Leo Grove <leo.grove@ssl.com>
2010-12-07 13:17:37 -0600

    deploying to herolu - added some gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 SslDocs::Application.configure do
+  APP_URL = "http://dev-station:3000"
   # Settings specified here will take precedence over those in config/environment.rb
 
   # In the development environment your application's code is reloaded on
@@ -12,15 +13,33 @@ SslDocs::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :cache
+
+  config.to_prepare do
+    OrderTransaction.gateway =
+      ActiveMerchant::Billing::AuthorizeNetGateway.new(
+        :login    => '9jFL5k9E',
+        :password => '8b3zEL5H69sN4Pth'
+      )
+    BillingProfile.password = "kama1jama1"
+  end
+
+  #config.log_level = Logger::INFO
+
+  GATEWAY_TEST_CODE=1.0
+  # END ActiveMerchant configuration
+
+
 end
 
