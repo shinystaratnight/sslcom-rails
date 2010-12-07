615de85ef7b0d626a8b643d0470d8fd3cfe7621b
Leo Grove <leo.grove@ssl.com>
2010-12-07 15:19:05 -0600

    adjusting index

--- config/initializers/generator_methods.rb
+++
@@ -1,7 +0,0 @@
-ActionView::Helpers::PrototypeHelper::JavaScriptGenerator::GeneratorMethods.module_eval do
-  def jquery(&block)
-    @lines << "(function($) {"
-    block.call(page)
-    @lines << "})(jQuery);"
-  end
-end
--- config/initializers/range.rb
+++
@@ -1,5 +0,0 @@
-Range.class_eval do
-  def to_friendly
-    to_s.gsub "..", "-"
-  end
-end
