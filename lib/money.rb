00edc204eca378e8a52e318ff1da3209f510afc6
Leo Grove <leo.grove@ssl.com>
2011-02-02 13:02:58 -0600

    made transition to Rails 3

---
+++ lib/money.rb
@@ -0,0 +1,6 @@
+Money.class_eval {
+  def ==(other_money)
+    return false unless defined? other_money.cents
+    cents == other_money.cents && bank.same_currency?(currency, other_money.currency)
+  end
+}
