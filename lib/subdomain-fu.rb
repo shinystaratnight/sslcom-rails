10479586a0f9b8da610d10a9fb346a893a0480c7
Leo Grove <leo.grove@ssl.com>
2011-10-21 17:18:13 -0500

    logging why error occurs on front page sometime

---
+++ lib/subdomain-fu.rb
@@ -0,0 +1,9 @@
+module SubdomainFu
+
+  def self.host_without_subdomain(host)
+    parts = host.split('.')
+    logger.error "host = #{host}" if parts[-(SubdomainFu.config.tld_size+1)..-1].blank?
+    parts[-(SubdomainFu.config.tld_size+1)..-1].join(".")
+  end
+
+end
