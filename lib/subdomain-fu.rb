5670dda4a37558081a4c6134ed5bb0a1338ab7a4
Leo Grove <leo.grove@ssl.com>
2011-10-21 19:39:34 -0500

    logging a join nil in subdomain-fu

--- lib/subdomain-fu.rb
+++ lib/subdomain-fu.rb
@@ -2,7 +2,7 @@ module SubdomainFu
 
   def self.host_without_subdomain(host)
     parts = host.split('.')
-    p "host = #{host}" if parts[-(SubdomainFu.config.tld_size+1)..-1].blank?
+    Rails.logger.error "host = #{host}" if parts[-(SubdomainFu.config.tld_size+1)..-1].blank?
     parts[-(SubdomainFu.config.tld_size+1)..-1].join(".")
   end
 
