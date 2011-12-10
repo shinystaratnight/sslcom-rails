86224ebbb0029fb04491750782af25bddb6b2373
Leo Grove <leo.grove@ssl.com>
2011-12-09 18:52:55 -0600

    fix upgrade issues associate with jquery 1.7. Added $.ajaxSettings.dataType = "json";

--- lib/clean_to_json.rb
+++
@@ -1,8 +0,0 @@
-# clean_to_json.rb
-module CleanToJson
-  def as_json(options = nil)
-    super(options).tap do |json|
-      json.delete_if{|k,v| v.nil?}.as_json unless options.try(:delete, :null)
-    end
-  end
-end
--- lib/hash.rb
+++
@@ -1,5 +0,0 @@
-class Hash
-  def delete_blank
-    delete_if{|k, v| v.blank? or v.instance_of?(Hash) && v.delete_blank.blank?}
-  end
-end
