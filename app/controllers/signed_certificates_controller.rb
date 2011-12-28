8023d3c32b36b20de97bceb5a72174d030450561
Leo Grove <leo.grove@ssl.com>
2011-12-27 18:40:18 -0600

    fix permission issue with bundle

--- app/controllers/signed_certificates_controller.rb
+++ app/controllers/signed_certificates_controller.rb
@@ -1,7 +1,7 @@
 class SignedCertificatesController < ApplicationController
   before_filter :new_signed_certificate_from_params, :on=>:create
   filter_access_to :all, :attribute_check=>true
-  filter_access_to :server_bundle, :require=>:read
+  filter_access_to :server_bundle, :require=>:show
 
   # DELETE /signed_certificates/1
   # DELETE /signed_certificates/1.xml
