a67276aa8dc98d36d536d21747b036b93435eb8a
Leo Grove <leo@leogrove.com>
2013-12-08 16:15:59 -0600

    cleanup activation email

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -6,41 +6,42 @@ class UserNotifier < ActionMailer::Base
 
   def activation_instructions(user)
     @account_activation_url = register_url(user.perishable_token)
-    mail subject:       "SSL.com Activation Instructions",
+    mail subject:       "SSL.com user account activation instructions",
             from:          Settings.from_email.activations,
             to:    user.email
   end
 
   def activation_confirmation(user)
-    @root_url = root_url
-    mail subject:       "SSL.com Activation Complete",
+    @account_url = account_url
+    @login = user.login
+    mail subject:       "SSL.com user account activated",
             from:          Settings.from_email.activations,
             to:    user.email
   end
 
   def password_reset_instructions(user)
     @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
-    mail  subject:      "SSL.com Password Reset Instructions",
+    mail  subject:      "SSL.com user account password reset instructions",
              from:         Settings.from_email.activations,
              to:   user.email
   end
 
   def password_changed(user)
-    mail subject:       "SSL.com Account Password Changed",
+    mail subject:       "SSL.com user account password changed",
            from:          Settings.from_email.activations,
            to:    user.email
   end
 
   def email_changed(user, email)
      @user=user
-     mail  subject:      "SSL.com Account Email Address Changed",
+     mail  subject:      "SSL.com user account email address changed",
               from:         Settings.from_email.activations,
               to:   email
   end
 
   def username_reminder(user)
      @login = user.login
-     mail  subject:       "SSL.com Username Reminder",
+     mail  subject:       "SSL.com username reminder",
               from:          Settings.from_email.activations,
               to:    user.email
   end
@@ -63,11 +64,11 @@ class UserNotifier < ActionMailer::Base
     @sent_on     = Time.now
     @body[:user] = user
   end
-  
+
   def setup_sender_info
     @from       = "The #{Settings.community_name} Team <#{Settings.support_email}>"
     headers     "Reply-to" => "#{Settings.support_email}"
     @content_type = "text/plain"
   end
-  
+
 end
