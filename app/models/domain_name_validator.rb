74f43ed875187dcbf21964d876e8b29bd23208ee
Leo Grove <leo@leogrove.com>
2014-07-08 12:10:01 -0500

    fixed domain_name_validator to use gtlds

--- app/models/domain_name_validator.rb
+++ app/models/domain_name_validator.rb
@@ -1,17 +1,17 @@
 class DomainNameValidator < ActiveModel::EachValidator
 
-  DOMAIN_NAME_FORMAT = Regexp.new('[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$',
-                          Regexp::IGNORECASE+Regexp::EXTENDED)
+  DOMAIN_NAME_FORMAT = Regexp.new('[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$', Regexp::IGNORECASE+Regexp::EXTENDED)
+  GTLDS = Regexp.new('\.('+CertificateContent::ICANN_TLDS.join("|")+')$', Regexp::IGNORECASE+Regexp::EXTENDED)
 
   def validate_each(record, attribute, value)
     if value.is_a? Array
       value.each do |val|
-        record.errors[attribute] << (options[:message] || "#{val} is not a valid domain name") unless
-            val =~ DOMAIN_NAME_FORMAT
+        record.errors[attribute] << (options[:message] || "#{val} is not a valid domain name") if
+            ((val =~ DOMAIN_NAME_FORMAT) && (val =~ GTLDS)).blank?
       end
     else
-      record.errors[attribute] << (options[:message] || "#{value} is not a valid domain name") unless
-          value =~ DOMAIN_NAME_FORMAT
+      record.errors[attribute] << (options[:message] || "#{value} is not a valid domain name") if
+          ((value =~ DOMAIN_NAME_FORMAT) && (value =~ GTLDS)).blank?
     end
   end
 end
