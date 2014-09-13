6fd959ff3b3656cf156af3e41a60d2fa81a519e2
Leo Grove <leo@leogrove.com>
2014-09-13 16:23:46 -0500

    migrate to idevaffiliate

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -122,7 +122,7 @@ module OrdersHelper
   def confirm_affiliate_sale
     if @order.ext_affiliate_name=="shareasale" && !@order.ext_affiliate_credited?
       @order.toggle! :ext_affiliate_credited
-      "<img src=\"https://shareasale.com/sale.cfm?amount=#{@order.final_amount.to_s}&tracking=#{@order.reference_number}&transtype=sale&merchantID=#{@order.ext_affiliate_id}\" width=\"1\" height=\"1\"><img border=\"0\" src=\"https://www.ssl.com/affiliate/program.php?profile=72198&idev_saleamt=#{@order.final_amount.to_s}&idev_ordernum=#{@order.reference_number}&coupon_code=#{@order.discounts.last.ref}\" width=\"1\" height=\"1\">".html_safe
+      "<img src=\"https://shareasale.com/sale.cfm?amount=#{@order.final_amount.to_s}&tracking=#{@order.reference_number}&transtype=sale&merchantID=#{@order.ext_affiliate_id}\" width=\"1\" height=\"1\"><img border=\"0\" src=\"https://www.ssl.com/affiliate/program.php?profile=72198&idev_saleamt=#{@order.final_amount.to_s}&idev_ordernum=#{@order.reference_number}#{"&coupon_code="+@order.discounts.last.ref unless @order.discounts.empty?}\" width=\"1\" height=\"1\">".html_safe
     end
     # if !@order.ext_affiliate_credited?
     #   @order.toggle! :ext_affiliate_credited
