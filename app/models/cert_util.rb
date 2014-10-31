8a80881b076cecb363f7a897bdf8328001f4a470
Leo Grove <leo@leogrove.com>
2014-10-31 17:34:54 -0500

    fixes

--- app/models/cert_util.rb
+++ app/models/cert_util.rb
@@ -1,9 +1,9 @@
 class CertUtil
-  CONVERT_PKCS7=->(pem, pkcs7, chain){%x"openssl crl2pkcs7 -nocrl -certfile #{pem} -out #{pkcs7} -certfile #{chain}"}
+  CONVERT_PEM_TO_PKCS7=->(pem, pkcs7, chain){%x"openssl crl2pkcs7 -nocrl -certfile #{pem} -out #{pkcs7} -certfile #{chain}"}
   DECODE_CERTIFICATE=->(cert_file, format){%x"openssl #{format} -in #{cert_file} -text -noout #{'-print_certs' if format=='pkcs7'}"}
 
   def self.pem_to_pkcs7(pem, chain, out)
-    CONVERT_PKCS7.call pem, out, chain
+    CONVERT_PEM_TO_PKCS7.call pem, out, chain
   end
 
   def self.decode_certificate(cert_file,format)
