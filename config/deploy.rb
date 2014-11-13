f9bfac2ddfec5cc81bcddae170870c9e803ddf64
Leo Grove <leo@leogrove.com>
2014-11-13 12:01:16 -0600

    reorganized the download certificate links

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,9 +26,9 @@ default_run_options[:pty] = true
 
 set :application, "ssl_com"
 # set :domain, '54.204.18.222' #WP
-set :domain, '184.73.226.63' #production
+# set :domain, '184.73.226.63' #production
 #set :domain, 'staging2.ssl.com' #development
-# set :domain, '54.83.39.189' # comodo extractor
+set :domain, '54.83.39.189' # comodo extractor
 
 # set :application, "sws-test.sslpki.com" # test api
 # set :application, "sws.sslpki.com" # api
