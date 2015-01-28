b802155dfd139191427ff26dfc3d0680efc59084
Leo Grove <leo@leogrove.com>
2015-01-27 19:01:10 -0600

    remove uniqueness on email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   attr_accessible :login, :email, :password, :password_confirmation,
     :openid_identifier, :status
   attr_readonly :login unless MIGRATING_FROM_LEGACY
-  validates :email, uniqueness: true, email: true
+  validates :email, email: true #,uniqueness: true #TODO look at impact on checkout
 
   acts_as_authentic do |c|
     c.validate_email_field = false
