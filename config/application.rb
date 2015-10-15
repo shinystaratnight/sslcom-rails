d38933acc85331b076c9a0f7007e1fcbe9d7dab3
Leo Grove <leo@leogrove.com>
2015-10-15 13:37:41 -0500

    certassure white label

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.setup
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 CLIENT_OPTIONS=["ssl.com","certassure"]
-DEPLOYMENT_CLIENT=CLIENT_OPTIONS[0]
+DEPLOYMENT_CLIENT=CLIENT_OPTIONS[1]
 
 module SslCom
   class Application < Rails::Application
