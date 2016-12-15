3a700b2a9cd6a3234505584fcca2e91a465bbb8c
GINA FILIPPOV <gina@mobirobo.com>
2016-12-14 17:46:38 -0800

    [Multitenant][SSL-30] Test Suite - billing for certificate orders workflow.

--- test/factories/ssl_accounts.rb
+++ test/factories/ssl_accounts.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
   factory :ssl_account do
   end
+
+  trait :billing_profile do
+    after(:create) do |ssl|
+      ssl.billing_profiles << create(:billing_profile)
+    end
+  end
 end
