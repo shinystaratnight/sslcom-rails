02da6b833d48c5ecb541ad731652040c4504f3d5
Leo Grove <leo@ssl.com>
2016-12-21 13:01:14 -0600

    1) added PRODUCTS to top links 2) added wildcard and nonwildcard test certs

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,8 @@ default_run_options[:pty] = true
 
 set :application, "ssl_com"
 # set :domain, '54.204.18.222' #WP development
-set :domain, '54.91.78.195' #Rails 4 staging
+#set :domain, '54.91.78.195' #Rails 4 staging
+set :domain, '54.91.7.32' #new Rails 4 production
 #set :domain, '184.73.226.63' #Rails 4 production
 #set :domain, 'staging2.ssl.com' #development
 # set :domain, '54.83.39.189' # comodo extractor
@@ -57,7 +58,7 @@ set :deploy_via, :remote_cache
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
 
 set :user, "ubuntu"
-set :branch, "ssl-34-friendly-name-slug-url-leo"
+set :branch, "ssl-30-test-suite-billing"
 set :use_sudo, false
 
 role :cache, domain
