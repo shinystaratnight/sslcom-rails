c93c58b1b248b430e8d4336fa25eaea218a3af75
GINA FILIPPOV <gina@mobirobo.com>
2017-01-19 16:46:03 -0800

    [Roles][SSL-44] Rake task - update to include creation of new roles and clearing vestigial roles from roles and assignments tables.

--- bin/populate_roles_team_update.rb
+++ bin/populate_roles_team_update.rb
@@ -9,7 +9,6 @@ account_admin_role.update(name: 'owner') if account_admin_role
 
 ssl_user_role = Role.find_by(name: 'ssl_user')
 ssl_user_role.update(name: 'account_admin') if ssl_user_role
-
 #
 # Populate users table: find the first ssl_account user owns and update
 # user's main_ssl_account to that id.
@@ -17,4 +16,33 @@ ssl_user_role.update(name: 'account_admin') if ssl_user_role
 User.all.each do |user|
   ssl = user.owned_ssl_account
   user.update(main_ssl_account: ssl.id) if ssl && user.main_ssl_account.nil?
+end
+#
+# Create 3 new roles in the roles table.
+#
+['billing', 'validations', 'installer'].each do |name|
+  Role.create(name: name)
+end
+#
+# Cleanup roles table:
+# If any vestigial role produces zero associations, then delete it from the roles table.
+#
+vestigial_roles = [
+  'vetter',
+  'Account Billing',
+  'certificates_approver',
+  'certificates_manager',
+  'prices_restricted',
+  'orders_requestor',
+  'orders_approver',
+  'orders_manager',
+  'admin',
+  'customer_admin'
+]
+
+vestigial_roles.each do |name|
+  role = Role.find_by(name: name)
+  if role && Assignment.where(role_id: role.id).empty?
+    role.destroy
+  end
 end
