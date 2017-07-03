6ceed64cfd7de5878d322a2a62efc2c61ed904a5
Leo Grove <leo@ssl.com>
2017-07-03 06:44:43 -0500

    1) added serial column to SignedCertificate 2) added model for api revoke call

--- app/models/api_certificate_revoke.rb
+++ app/models/api_certificate_revoke.rb
@@ -1,8 +1,25 @@
-class ApiCertificateRevoke < ApiCertificateRequest
-  validates :account_key, :secret_key, :reason, presence: true
-  validates :ref, presence: true
+class ApiCertificateRetrieve < ApiCertificateRequest
+  validates :account_key, :secret_key, :revocation_reason, presence: true
+  validates :ref, presence: true, unless: lambda{|c|c.serial}
+  validates :serial, presence: true, unless: lambda{|c|c.ref}
+  validates_presence_of :signed_certificate_exists
 
-  # return values
-  attr_accessor :status
+  attr_accessor :revocation_reason, :domains, :ext_status, :certificates, :order_status, :certificate_order,
+                :common_name, :subject_alternative_names, :effective_date, :expiration_date, :algorithm,
+                :site_seal_code, :domains_qty_purchased, :wildcard_qty_purchased, :description, :subscriber_agreement,
+                :order_date
+
+  def signed_certificate_exists
+      if self.ref
+        self.signed_certificate=
+            (self.api_requestable.certificate_orders.find_by_ref(self.ref) or
+            self.api_requestable.certificate_contents.find_by_ref(self.ref)).signed_certificate
+        errors[:ref] << "doesn't exist'" unless self.signed_certificate
+      elsif self.serial
+        self.signed_certificate=self.api_requestable.signed_certificates.find_by_serial(self.serial).last
+        errors[:serial] << "doesn't exist'" unless self.signed_certificate
+      end
+      self.signed_certificate
+  end
 
 end
