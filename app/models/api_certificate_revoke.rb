b0c3e45558857f902387e2c0f1a54aa1a52a62b8
Leo Grove <leo@ssl.com>
2017-07-22 23:28:05 -0500

    revoke api response

--- app/models/api_certificate_revoke.rb
+++ app/models/api_certificate_revoke.rb
@@ -2,4 +2,7 @@ class ApiCertificateRevoke < ApiCertificateRequest
   validates :account_key, :secret_key, :reason, presence: true
   validates :ref, presence: true
 
+  # return values
+  attr_accessor :status
+
 end
