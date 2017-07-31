ab040d95c0c208282d76a70eda861723918f30db
Leo Grove <leo@ssl.com>
2017-07-31 17:59:10 -0500

    changed deploy for staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,8 @@ default_run_options[:pty] = true
 
 set :application, "ssl_com"
 # set :domain, '54.204.18.222' #WP development
-# set :domain, '50.19.246.227' #Rails 4 staging
-set :domain, 'ra.sslpki.local' #Rails 4 production
+set :domain, '50.19.246.227' #Rails 4 staging
+# set :domain, 'ra.sslpki.local' #Rails 4 production
 #set :domain, 'staging2.ssl.com' #development
 # set :domain, '54.83.39.189' # comodo extractor
 
@@ -57,7 +57,7 @@ set :deploy_via, :remote_cache
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
 
 set :user, "ubuntu"
-set :branch, "master"
+set :branch, "staging"
 set :use_sudo, false
 
 role :cache, domain
