7f937c8342c7543d4e1eef8c58ad089eec5f0282
codelux <leo@ssl.com>
2017-11-24 10:08:10 -0600

    SSL.com CA API call

--- app/models/certificate_content.rb
+++ app/models/certificate_content.rb
@@ -464,6 +464,16 @@ class CertificateContent < ActiveRecord::Base
     end
   end
 
+  def subject_dn(options)
+    cert = options[:certificate] || self.certificate
+    dn=["CN=#{options[:common_name] || csr.common_name}"]
+    if cert.is_ov? or cert.is_ev?
+      dn << "O=#{registant.company_name}"
+    end
+    dn << options[:custom_fields] if options[:custom_fields]
+    dn.join(",")
+  end
+
   private
   
   def validate_domains?
