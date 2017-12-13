9cd184860f547e9841afc28682bd740b4cb31676
codelux <leo@ssl.com>
2017-12-13 15:09:48 -0600

    fixed API issue with joi

--- app/models/certificate_content.rb
+++ app/models/certificate_content.rb
@@ -572,11 +572,11 @@ class CertificateContent < ActiveRecord::Base
       dn << "O=#{options[:o] || registrant.company_name}"
       dn << "C=#{options[:c] || registrant.country}"
       if cert.is_ev?
-        dn << "serialNumber=#{options[:serial_number] || certificate_order.jois.last.company_number}"
-        dn << "2.5.4.15=#{options[:business_category] || certificate_order.jois.last.business_category}"
-        dn << "1.3.6.1.4.1.311.60.2.1.1=#{options[:joi_locality] || certificate_order.jois.last.city}"
-        dn << "1.3.6.1.4.1.311.60.2.1.2=#{options[:joi_state] || certificate_order.jois.last.state}"
-        dn << "1.3.6.1.4.1.311.60.2.1.3=#{options[:joi_country] || certificate_order.jois.last.country}"
+        dn << "serialNumber=#{options[:serial_number] || certificate_order.jois.last.try(:company_number)}"
+        dn << "2.5.4.15=#{options[:business_category] || certificate_order.jois.last.try(:business_category)}"
+        dn << "1.3.6.1.4.1.311.60.2.1.1=#{options[:joi_locality] || certificate_order.jois.last.try(:city)}"
+        dn << "1.3.6.1.4.1.311.60.2.1.2=#{options[:joi_state] || certificate_order.jois.last.try(:state)}"
+        dn << "1.3.6.1.4.1.311.60.2.1.3=#{options[:joi_country] || certificate_order.jois.last.try(:country)}"
       end
     end
     dn << options[:custom_fields] if options[:custom_fields]
