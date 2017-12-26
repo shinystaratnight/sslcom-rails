cd8807205c087599cb25e8dee25f8bd05992b164
codelux <leo@ssl.com>
2017-12-26 12:52:11 -0600

    Subscriber Agreement fix; ca_certificate_id fix

--- app/models/certificate_content.rb
+++ app/models/certificate_content.rb
@@ -57,7 +57,7 @@ class CertificateContent < ActiveRecord::Base
 
   serialize :domains
 
-  validates_presence_of :server_software_id, :signing_request,
+  validates_presence_of :server_software_id, :signing_request, :agreement,
     :if => "certificate_order_has_csr && !ajax_check_csr && Settings.require_server_software_w_csr_submit"
   validates_format_of :signing_request, :with=>SIGNING_REQUEST_REGEX,
     :message=> 'contains invalid characters.',
