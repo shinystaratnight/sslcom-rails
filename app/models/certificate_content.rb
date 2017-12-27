a6f5831f27f7d59eb682dc8fe941ac8582dfa569
codelux <leo@ssl.com>
2017-12-27 05:41:37 -0600

    fix the subscriber agreement

--- app/models/certificate_content.rb
+++ app/models/certificate_content.rb
@@ -57,7 +57,7 @@ class CertificateContent < ActiveRecord::Base
 
   serialize :domains
 
-  validates_presence_of :server_software_id, :signing_request, :agreement,
+  validates_presence_of :server_software_id, :signing_request, # :agreement, # need to test :agreement out on reprocess and api submits
     :if => "certificate_order_has_csr && !ajax_check_csr && Settings.require_server_software_w_csr_submit"
   validates_format_of :signing_request, :with=>SIGNING_REQUEST_REGEX,
     :message=> 'contains invalid characters.',
@@ -426,7 +426,7 @@ class CertificateContent < ActiveRecord::Base
       CertificateContent::CONTACT_ROLES.map{|role|self.send "#{role}_contact"}
     else
       [].tap{|c_tmp|CertificateContent::CONTACT_ROLES.each {|r|
-        c_tmp << CertificateContact.new(contactable: self, country: self.registrant.country, roles: [r])}}
+        c_tmp << CertificateContact.new(contactable: self, country: self.registrant.try(:country), roles: [r])}}
     end
   end
 
