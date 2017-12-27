a87f0c871d1a5f61c1c3a03777bb5d526fe1548c
Gina Filippov <gina@fullstacklabs.co>
2017-12-27 13:47:14 -0800

    [SP-145] Error - ActiveRecord::StatementInvalid Mysql2::Error: Incorrect string value: '\xE9\x99\x88' for column 'first_name'.

---
+++ db/migrate/20171227203946_update_encoding_to_invoices.rb
@@ -0,0 +1,27 @@
+class UpdateEncodingToInvoices < ActiveRecord::Migration
+  def db
+    ActiveRecord::Base.connection
+  end
+  
+  def up
+    table = "invoices"
+    execute "ALTER TABLE `#{table}` CHARACTER SET = utf8mb4;"
+    db.columns(table).each do |column|
+      case column.sql_type
+        when /([a-z]*)text/i
+          execute "ALTER TABLE `#{table}` CHANGE `#{column.name}` `#{column.name}` #{$1.upcase}TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+        when /((?:var)?char)\(([0-9]+)\)/i
+          # InnoDB has a maximum index length of 767 bytes, so for utf8 or utf8mb4
+          # columns, you can index a maximum of 255 or 191 characters, respectively.
+          # If you currently have utf8 columns with indexes longer than 191 characters,
+          # you will need to index a smaller number of characters.
+          indexed_column = db.indexes(table).any? { |index| index.columns.include?(column.name) }
+
+          sql_type = (indexed_column && $2.to_i > 191) ? "#{$1}(191)" : column.sql_type.upcase
+          default = (column.default.nil?) ? '' : "DEFAULT '#{column.default}'"
+          null = (column.null) ? '' : 'NOT NULL'
+          execute "ALTER TABLE `#{table}` CHANGE `#{column.name}` `#{column.name}` #{sql_type} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci #{default} #{null};"
+      end
+    end
+  end
+end
