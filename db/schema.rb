5e4c3a2f2cd0eca8ab099f662c2bb31293fd19ea
Gina Filippov <gina@fullstacklabs.co>
2017-12-27 14:43:57 -0800

    DB migration - convert all tables with latin1 encoding to UTF8 encoding.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207223422) do
+ActiveRecord::Schema.define(version: 20171227220610) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name",        limit: 255
