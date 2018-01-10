7acbac173f46e9618bdd7ae277b705854038c840
codelux <leo@ssl.com>
2018-01-10 10:18:17 -0600

    schema change; sandbox criteria change

--- db/schema.rb
+++ db/schema.rb
@@ -135,18 +135,6 @@ ActiveRecord::Schema.define(version: 20180105231017) do
 
   add_index "billing_profiles", ["ssl_account_id"], name: "index_billing_profile_on_ssl_account_id", using: :btree
 
-  create_table "blocklist", force: :cascade do |t|
-    t.string   "type",        limit: 255
-    t.string   "domain",      limit: 255
-    t.integer  "validation",  limit: 4
-    t.string   "status",      limit: 255
-    t.string   "reason",      limit: 255
-    t.string   "description", limit: 255
-    t.text     "notes",       limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "blocklists", force: :cascade do |t|
     t.string   "type",        limit: 255
     t.string   "domain",      limit: 255
@@ -177,16 +165,6 @@ ActiveRecord::Schema.define(version: 20180105231017) do
   add_index "ca_api_requests", ["api_requestable_id", "api_requestable_type"], name: "index_ca_api_requests_on_api_requestable", using: :btree
   add_index "ca_api_requests", ["id", "api_requestable_id", "api_requestable_type", "type", "created_at"], name: "index_ca_api_requests_on_type_and_api_requestable_and_created_at", using: :btree
   add_index "ca_api_requests", ["id", "api_requestable_id", "api_requestable_type", "type"], name: "index_ca_api_requests_on_type_and_api_requestable", unique: true, using: :btree
-
-  create_table "caa_check", force: :cascade do |t|
-    t.integer  "checkable_id",   limit: 4
-    t.string   "checkable_type", limit: 255
-    t.string   "domain",         limit: 255
-    t.string   "request",        limit: 255
-    t.text     "result",         limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "caa_checks", force: :cascade do |t|
     t.integer  "checkable_id",   limit: 4
@@ -810,19 +788,6 @@ ActiveRecord::Schema.define(version: 20180105231017) do
     t.integer  "role_id",       limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-  end
-
-  create_table "physical_token", force: :cascade do |t|
-    t.integer  "certificate_order_id",  limit: 4
-    t.integer  "signed_certificate_id", limit: 4
-    t.string   "tracking_number",       limit: 255
-    t.string   "shipping_method",       limit: 255
-    t.string   "activation_pin",        limit: 255
-    t.string   "manufacturer",          limit: 255
-    t.string   "model_number",          limit: 255
-    t.string   "serial_number",         limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "physical_tokens", force: :cascade do |t|
