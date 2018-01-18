1fcda73a9cfe974776af91612399f845579a2097
codelux <leo@ssl.com>
2018-01-18 15:51:53 -0600

    minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user, :is_reseller, :cookies, :current_website,
     :cart_contents, :cart_products, :certificates_from_cookie, "is_iphone?", "hide_dcv?", :free_qty_limit,
     "hide_documents?", "hide_both?", "hide_validation?"
-  before_filter :set_database, unless: "request.host=~/^www\.ssl\.com/ || request.host=~/^sws\.sslpki\.com/ || request.host=~/^reseller\.ssl\.com/ || Rails.env.test?"
+  before_filter :set_database, unless: "request.host=~/^www\.ssl/ || request.host=~/^sws\.sslpki/ || request.host=~/^reseller\.ssl/ || Rails.env.test?"
   before_filter :set_mailer_host
   before_filter :detect_recert, except: [:renew, :reprocess]
   before_filter :set_current_user
