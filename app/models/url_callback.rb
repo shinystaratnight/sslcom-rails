46980ab23a6c22923d092ecf2f79c967a184fb7c
codelux <leo@ssl.com>
2018-03-01 16:27:44 -0600

    allow for custom content-type in callback

--- app/models/url_callback.rb
+++ app/models/url_callback.rb
@@ -14,6 +14,7 @@ class UrlCallback < ActiveRecord::Base
   #     {in: AUTH.values, message: "needs to be one of the following: #{AUTH.values.join(', ')}"}
   validate    :auth_validation, if: lambda {|cb|cb.auth}
   serialize   :parameters
+  serialize   :headers
   serialize   :auth
 
 
@@ -41,21 +42,25 @@ class UrlCallback < ActiveRecord::Base
   end
 
   def perform_callback(options={})
-    uri = URI.parse(self.url)
-    req = method==METHODS[:post] ? Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json') :
-              Net::HTTP::Get.new(uri)
-    req.basic_auth parameters[:username], parameters[:password] if auth
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req.body = if parameters['certificate_hook']
-      cert_param = parameters['certificate_hook']
-      parameters.delete('certificate_hook')
-      parameters.merge(cert_param=>options[:certificate_hook])
-    else
-      parameters.merge(certificate_hook: options[:certificate_hook])
-    end.to_json
-    res = http.request(req)
-    return req, res
+    begin
+      uri = URI.parse(self.url)
+      req = method==METHODS[:post] ? Net::HTTP::Post.new(uri, 'Content-Type' =>
+        (headers and headers["content-type"]) ? headers["content-type"] : 'application/json') : Net::HTTP::Get.new(uri)
+      req.basic_auth auth["basic"]["username"], auth["basic"]["username"] if (auth and auth["basic"])
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      req.body = if parameters['certificate_hook']
+        cert_param = parameters['certificate_hook']
+        parameters.delete('certificate_hook')
+        parameters.merge(cert_param=>options[:certificate_hook])
+      else
+        parameters.merge(certificate_hook: options[:certificate_hook])
+      end.to_json
+      res = http.request(req)
+      return req, res
+    rescue Exception=>e
+      return false
+    end
   end
 end
