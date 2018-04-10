3d2238dbb324d93640327bf1f7179078acd9503d
codelux <leo@ssl.com>
2018-04-10 14:39:21 -0500

    site seal code in api return is causing issues

--- app/controllers/api/v1/api_certificate_requests_controller.rb
+++ app/controllers/api/v1/api_certificate_requests_controller.rb
@@ -1072,11 +1072,11 @@ class Api::V1::ApiCertificateRequestsController < Api::V1::APIController
       result.effective_date = acr.signed_certificate.effective_date
       result.expiration_date = acr.signed_certificate.expiration_date
       result.algorithm = acr.signed_certificate.is_SHA2? ? "SHA256" : "SHA1"
-      result.site_seal_code = ERB::Util.json_escape(render_to_string(
-                                                         partial: 'site_seals/site_seal_code.html.haml',
-                                                         locals: {co: acr},
-                                                         layout: false
-                                                     ))
+      # result.site_seal_code = ERB::Util.json_escape(render_to_string(
+      #                                                    partial: 'site_seals/site_seal_code.html.haml',
+      #                                                    locals: {co: acr},
+      #                                                    layout: false
+      #                                                ))
     elsif (acr.csr)
       result.certificates = nil
       result.common_name = acr.csr.common_name
