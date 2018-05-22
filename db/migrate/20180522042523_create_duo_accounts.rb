d5fbf1833346807fd4311b7793c34151ec817fed
sarlongda <34284034+sarlongda@users.noreply.github.com>
2018-05-22 12:34:19 +0700

    duo_accounts migration Fix

--- db/migrate/20180522042523_create_duo_accounts.rb
+++ db/migrate/20180522042523_create_duo_accounts.rb
@@ -2,7 +2,7 @@ class CreateDuoAccounts < ActiveRecord::Migration
   def change
     create_table :duo_accounts do |t|
       t.references  :ssl_account
-      t.string      :duo_ikey, :duo_skey, :duo_akey, :duo_hostname, :encrypted_duo_ikey, :encrypted_duo_skey, :encrypted_duo_akey, :encrypted_duo_hostname, :encrypted_duo_accounts_salt, :encrypted_duo_accounts_iv
+      t.string      :duo_ikey, :duo_skey, :duo_akey, :duo_hostname, :encrypted_duo_ikey, :encrypted_duo_skey, :encrypted_duo_akey, :encrypted_duo_hostname, :encrypted_duo_ikey_salt, :encrypted_duo_ikey_iv, :encrypted_duo_skey_salt, :encrypted_duo_skey_iv, :encrypted_duo_akey_salt, :encrypted_duo_akey_iv, :encrypted_duo_hostname_salt, :encrypted_duo_hostname_iv
       t.timestamps  null: false
     end
   end
