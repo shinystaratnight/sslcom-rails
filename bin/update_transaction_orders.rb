1ad35180fef9fe81a77bbeca20f48330d6527eb9
codelux <leo@ssl.com>
2018-05-28 11:33:31 -0500

    optimize

--- bin/update_transaction_orders.rb
+++ bin/update_transaction_orders.rb
@@ -2,6 +2,6 @@
 # 
 # Update cents attribute based on the stored amount (old_amount) value
 #
-OrderTransaction.all.each do |ot|
+OrderTransaction.find_each do |ot|
   ot.update(cents: (ot.old_amount * 100)) unless ot.old_amount.blank?
 end
