3f8c60492c7874e28d6fdc86f69f940ced5f025a
codelux <leo@ssl.com>
2018-07-02 17:52:56 -0500

    migration

--- db/migrate/20180629182134_add_ssl_account_id_to_cas_certificates.rb
+++ db/migrate/20180629182134_add_ssl_account_id_to_cas_certificates.rb
@@ -5,5 +5,6 @@ class AddSslAccountIdToCasCertificates < ActiveRecord::Migration
     add_index :cas_certificates, :ssl_account_id
 
     remove_column :cas, :profile_type, :string
+    remove_column :certificates, :ca_certificate_id, :integer
   end
 end
