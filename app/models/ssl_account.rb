edd41ff3111d75ef83008d2be06cdcccf47ead78
codelux <leo@ssl.com>
2018-07-29 12:01:47 -0500

    filter to get certificate_contents mapped to SSL.com CA

--- app/models/ssl_account.rb
+++ app/models/ssl_account.rb
@@ -39,7 +39,11 @@ class SslAccount < ActiveRecord::Base
   has_many  :all_saved_contacts, as: :contactable, class_name: 'Contact', dependent: :destroy
   has_many  :cdns
   has_many  :tags
-  has_many :certificate_names, through: :certificate_contents
+  has_many :certificate_names, through: :certificate_contents do
+    def sslcom
+      where.not certificate_contents: {ca_id: nil}
+    end
+  end
   has_many :domain_control_validations, through: :certificate_names do
     def sslcom
       where.not certificate_contents: {ca_id: nil}
