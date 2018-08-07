6dd957ae0281acb6a4badd947ba208a5e55dc9b6
codelux <leo@ssl.com>
2018-08-07 17:30:06 -0500

    cache preferences

--- app/models/certificate_order.rb
+++ app/models/certificate_order.rb
@@ -2012,7 +2012,7 @@ class CertificateOrder < ActiveRecord::Base
   end
 
   # cron job that flags unused certificate_order credits as expired after a period of time (1 year)
-  def self.expire_credits(options)
+  def self.expire_credits(options={})
     Website.sandbox_db.use_database if options[:db]=="sandbox"
     CertificateOrder.unflagged_expired_credits.update_all(is_expired: true)
     SystemAudit.create(owner: nil, target: nil,
