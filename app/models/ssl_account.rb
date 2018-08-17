df45767a62617b6f591ee50c7e859842aa1afc89
sarlongda <34284034+sarlongda@users.noreply.github.com>
2018-08-17 23:57:52 +0700

    Rebase from staging

--- app/models/ssl_account.rb
+++ app/models/ssl_account.rb
@@ -22,6 +22,7 @@ class SslAccount < ActiveRecord::Base
   has_many  :site_seals, through: :certificate_orders
   has_many  :certificate_contents, through: :certificate_orders
   has_many  :domains, :dependent => :destroy
+  has_many  :csrs, through: :certificate_contents
   has_many  :signed_certificates, through: :certificate_contents do
     def expired
       where{expiration_date < Date.today}
