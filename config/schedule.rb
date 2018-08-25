40abca9536dfaa4aa45733b28f634f4cc6f8553a
codelux <leo@ssl.com>
2018-08-24 23:05:18 -0500

    fixes for views

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,10 +21,10 @@
 
 job_type :bundle_exec,  "cd :path && bundle exec script/rails runner -e :environment ':task' :output"
 
-every 12.hours, at: "12am" do
-  bundle_exec "CertificateOrder.expire_credits(db: 'sandbox')"
-  #bundle_exec "Malware.update"
-end
+# every 12.hours, at: "12am" do
+#   bundle_exec "CertificateOrder.expire_credits(db: 'sandbox')"
+#   #bundle_exec "Malware.update"
+# end
 
 every 20.minutes do
   bundle_exec "CertificateOrder.retrieve_ca_certs(12.hours.ago, DateTime.now, db: 'sandbox')"
@@ -44,4 +44,8 @@ end
 
 every 1.day, at: "12:30pm" do
   bundle_exec "CertificateOrder.retrieve_ca_certs(30.days.ago, 15.days.ago, db: 'sandbox')"
+end
+
+every 1.minute do
+  bundle_exec "NotificationGroup.scan(db: 'sandbox')"
 end
