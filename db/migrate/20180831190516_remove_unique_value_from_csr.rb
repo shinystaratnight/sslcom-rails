5de66e4027e4354146dc6f53866f6bdc22d5d71c
codelux <leo@ssl.com>
2018-09-10 13:52:32 -0500

    remove index

--- db/migrate/20180831190516_remove_unique_value_from_csr.rb
+++ db/migrate/20180831190516_remove_unique_value_from_csr.rb
@@ -1,5 +1,6 @@
 class RemoveUniqueValueFromCsr < ActiveRecord::Migration
   def change
+    remove_index :csrs, [:public_key_sha1, :unique_value]
     remove_column :csrs, :unique_value, :string
   end
 end
