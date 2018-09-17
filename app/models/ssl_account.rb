7e2b29316da0ac58199940b8568137a7309041c5
codelux <leo@ssl.com>
2018-09-17 10:42:03 -0500

    fix assigning ca

--- app/models/ssl_account.rb
+++ app/models/ssl_account.rb
@@ -9,9 +9,6 @@ class SslAccount < ActiveRecord::Base
             before_add: Proc.new { |p, d|
                 folder=Folder.find_by(default: true, ssl_account_id: p.id)
                 d.folder_id= folder.id unless folder.blank?
-            },
-            after_add: Proc.new { |p, d|
-                d.certificate_content.add_ca(p)
             } do
     def current
       where{workflow_state >>['new']}.first
