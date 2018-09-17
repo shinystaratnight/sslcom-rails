282d4c9c79a3033d431d9a6b498e74eb3714488a
codelux <leo@ssl.com>
2018-09-17 10:55:01 -0500

    do not do caa if not enabled

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -90,7 +90,7 @@ class ValidationsController < ApplicationController
                   dcv = cn.blank? ? nil : cn.domain_control_validations.last
                   value['attempted_on'] = dcv.blank? ? 'n/a' : dcv.created_at
 
-                  if cn.caa_passed
+                  if enable_caa && cn.try(:caa_passed)
                     value['caa_check'] = 'passed'
                   else
                     value['caa_check'] = 'failed'
