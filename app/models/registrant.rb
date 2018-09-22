c83e9eafb1aa1b549582e5c45225db6f0195412d
codelux <leo@ssl.com>
2018-09-22 14:54:55 -0500

    form changes; removed validations on registrant

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -4,49 +4,47 @@ class Registrant < Contact
   
   after_save :set_default_status
 
-  unless MIGRATING_FROM_LEGACY
-    validates_presence_of :contactable
-    # validates_acceptance_of :validation,
-    #   if: Proc.new {|r|
-    #     !r.reusable? && (
-    #       r.contactable &&
-    #       r.contactable.certificate_order &&
-    #       r.contactable.certificate_order.ssl_account.has_role?("reseller")
-    #     )
-    #   }
-    validates_presence_of :company_name, :address1, :city, :state, :postal_code, :country,
-      if: Proc.new{|r| !r.reusable? && r.contactable && r.contactable.certificate_order.certificate.requires_company_info?}
-    validates_presence_of :phone,
-      if: Proc.new{|r|
-        !r.reusable? && r.contactable && (
-          r.contactable.certificate_order.certificate.is_code_signing? ||
-          r.contactable.certificate_order.certificate.is_client_enterprise? ||
-          r.contactable.certificate_order.certificate.is_client_business?
-        )
-      }
-    validates_presence_of :title,
-      if: Proc.new{|r|
-        !r.reusable? && r.contactable && (
-          r.contactable.certificate_order.certificate.is_client_enterprise? ||
-          r.contactable.certificate_order.certificate.is_client_business?
-        )
-      }
-    validates_presence_of :email,
-      if: Proc.new{|r|
-        !r.reusable? && r.contactable &&
-        r.contactable.certificate_order.certificate.is_code_signing?
-      }
-    validates_presence_of :first_name, :last_name,
-      if: Proc.new{|r|
-        !r.reusable? && r.contactable && (
-          r.contactable.certificate_order.certificate.is_client_pro? ||
-          r.contactable.certificate_order.certificate.is_code_signing? ||
-          r.contactable.certificate_order.certificate.is_client_enterprise? ||
-          r.contactable.certificate_order.certificate.is_client_business?
-        )
-      }
-  end
-  
+  validates_presence_of :contactable
+  # validates_acceptance_of :validation,
+  #   if: Proc.new {|r|
+  #     !r.reusable? && (
+  #       r.contactable &&
+  #       r.contactable.certificate_order &&
+  #       r.contactable.certificate_order.ssl_account.has_role?("reseller")
+  #     )
+  #   }
+  # validates_presence_of :company_name, :address1, :city, :state, :postal_code, :country,
+  #   if: Proc.new{|r| !r.reusable? && r.contactable && r.contactable.certificate_order.certificate.requires_company_info?}
+  # validates_presence_of :phone,
+  #   if: Proc.new{|r|
+  #     !r.reusable? && r.contactable && (
+  #       r.contactable.certificate_order.certificate.is_code_signing? ||
+  #       r.contactable.certificate_order.certificate.is_client_enterprise? ||
+  #       r.contactable.certificate_order.certificate.is_client_business?
+  #     )
+  #   }
+  # validates_presence_of :title,
+  #   if: Proc.new{|r|
+  #     !r.reusable? && r.contactable && (
+  #       r.contactable.certificate_order.certificate.is_client_enterprise? ||
+  #       r.contactable.certificate_order.certificate.is_client_business?
+  #     )
+  #   }
+  # validates_presence_of :email,
+  #   if: Proc.new{|r|
+  #     !r.reusable? && r.contactable &&
+  #     r.contactable.certificate_order.certificate.is_code_signing?
+  #   }
+  # validates_presence_of :first_name, :last_name,
+  #   if: Proc.new{|r|
+  #     !r.reusable? && r.contactable && (
+  #       r.contactable.certificate_order.certificate.is_client_pro? ||
+  #       r.contactable.certificate_order.certificate.is_code_signing? ||
+  #       r.contactable.certificate_order.certificate.is_client_enterprise? ||
+  #       r.contactable.certificate_order.certificate.is_client_business?
+  #     )
+  #   }
+
   validates :address1, :city, :state, :country, :postal_code, :email, presence: true,
     if: proc { |r| r.reusable? && (r.organization? || r.individual?) }
   validates :company_name, :phone, presence: true, if: proc { |r| r.reusable? && r.organization? }
