0042945f1ec27fc08b76641b06bbb6ddc7535b01
codelux <leo@ssl.com>
2018-09-24 14:27:59 -0500

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -1394,6 +1394,7 @@ ActiveRecord::Schema.define(version: 20180920032908) do
     t.integer  "registered_agent_id",       limit: 4
   end
 
+  add_index "signed_certificates", ["ca_id"], name: "index_signed_certificates_on_ca_id", using: :btree
   add_index "signed_certificates", ["common_name", "strength"], name: "index_signed_certificates_on_3_cols", using: :btree
   add_index "signed_certificates", ["common_name"], name: "index_signed_certificates_on_common_name", using: :btree
   add_index "signed_certificates", ["csr_id"], name: "index_signed_certificates_on_csr_id", using: :btree
