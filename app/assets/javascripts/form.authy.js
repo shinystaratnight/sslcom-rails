078f7602309a569e71dbb2f1a862918860794388
Gina Filippov <gina@fullstacklabs.co>
2019-02-20 10:56:41 -0800

    Fix error country code broken - Do not bind onchange action if saved contacts dropdown is not rendered.

--- app/assets/javascripts/form.authy.js
+++ app/assets/javascripts/form.authy.js
@@ -804,36 +804,40 @@
       return liElement.className = "active";
     };
     setupEvents = function(countriesInput, listId) {
+      var saved_contacts_sel = document.getElementById("saved_contacts");
       if (!countriesInput) {
         return;
       }
-      document.getElementById("saved_contacts").onchange = function(e) {
-        var activeElement, caId, countriesArr, contacts, i;
-        caId = "countries-autocomplete-" + listId;
-        countriesArr = document.getElementById(caId).getElementsByTagName("li");
-        contacts = document.getElementById("saved_contacts");
-        selectedCode = contacts.options[contacts.selectedIndex].getAttribute("data-country_code");
-        i = countriesArr.length - 1;
-        
-        if (selectedCode == null 
-          || (typeof selectedCode === "undefined")
-          || (selectedCode && (selectedCode.trim().length == 0 || selectedCode.trim().length == 10))
-        ) {
-          selectedCode = countriesList[0].code;
-        }  
-        if (selectedCode) {
-          while (i >= 0) {
-            countryCode = countriesArr[i].getAttribute("rel");
-            if (selectedCode === countryCode) {
-              activeElement = countriesArr[i]
-              setActive(activeElement);
-              self.autocomplete(activeElement, false);
-              return false;
+
+      if (saved_contacts_sel) {
+        saved_contacts_sel.onchange = function() {
+          var activeElement, caId, countriesArr, contacts, i;
+          caId = "countries-autocomplete-" + listId;
+          countriesArr = document.getElementById(caId).getElementsByTagName("li");
+          contacts = saved_contacts_sel;
+          selectedCode = contacts.options[contacts.selectedIndex].getAttribute("data-country_code");
+          i = countriesArr.length - 1;
+          
+          if (selectedCode == null
+            || (typeof selectedCode === "undefined")
+            || (selectedCode && (selectedCode.trim().length == 0 || selectedCode.trim().length == 10))
+          ) {
+            selectedCode = countriesList[0].code;
+          }  
+          if (selectedCode) {
+            while (i >= 0) {
+              countryCode = countriesArr[i].getAttribute("rel");
+              if (selectedCode === countryCode) {
+                activeElement = countriesArr[i]
+                setActive(activeElement);
+                self.autocomplete(activeElement, false);
+                return false;
+              }
+              i--;
             }
-            i--;
           }
-        }
-      };
+        };
+      }
 
       countriesInput.onblur = function(event) {
         return processKey13(listId);
