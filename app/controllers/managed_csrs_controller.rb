16b7b2e8ae6b7fc9ad91737a1c03734caf03410b
superRoR <isgloballove1981@gmail.com>
2019-02-28 05:51:50 +0700

    not logged in user to allow to visit managed_csr/new page and generating CSR and PrivateKey

--- app/controllers/managed_csrs_controller.rb
+++ app/controllers/managed_csrs_controller.rb
@@ -1,6 +1,7 @@
 class ManagedCsrsController < ApplicationController
-  before_filter :require_user, :set_ssl_slug
+  before_filter :require_user, :set_ssl_slug, except: [:new, :add_generated_csr]
   before_filter :set_row_page, only: [:index]
+  before_filter :set_sign_hash_algorithms, only: [:new]
 
   def index
     @csrs = (current_user.ssl_account.all_csrs).paginate(@p)
@@ -16,8 +17,11 @@ class ManagedCsrsController < ApplicationController
       co_token = CertificateOrderToken.find_by_token(params[:cert_token])
       @error_cert_ref = co_token.certificate_order.ref
     end
-    @csr = ManagedCsr.new
-    @cert_orders = current_user.ssl_account.certificate_orders.unused.map{|cert_order| [cert_order.ref, cert_order.id]}
+
+    unless current_user.blank?
+      @csr = ManagedCsr.new
+      @cert_orders = current_user.ssl_account.certificate_orders.unused.map{|cert_order| [cert_order.ref, cert_order.id]}
+    end
   end
 
   def create
@@ -63,33 +67,46 @@ class ManagedCsrsController < ApplicationController
   def add_generated_csr
     returnObj = {}
 
-    if params[:csr_id]
-      @csr = current_user.ssl_account.all_csrs.find_by(id: params[:csr_id])
-      @csr.body = params[:csr]
-      @csr.friendly_name = params[:friendly_name] && !params[:friendly_name].empty? ? params[:friendly_name] : nil
+    if params[:is_logged_in] == 'true'
+      if current_user
+        if params[:csr_id]
+          @csr = current_user.ssl_account.all_csrs.find_by(id: params[:csr_id])
+          @csr.body = params[:csr]
+          @csr.friendly_name = params[:friendly_name] && !params[:friendly_name].empty? ? params[:friendly_name] : nil
 
-      if @csr.save
-        returnObj['status'] = 'true'
-        returnObj['csr_ref'] = @csr.ref
-        returnObj['hash'] = @csr.public_key_sha1
+          if @csr.save
+            returnObj['status'] = 'true'
+            returnObj['csr_ref'] = @csr.ref
+            returnObj['hash'] = @csr.public_key_sha1
+          else
+            returnObj['status'] = 'There was a problem adding this CSR to the CSR Manager.'
+          end
+        else
+          @csr = ManagedCsr.new
+          @csr.friendly_name = params[:friendly_name] && !params[:friendly_name].empty? ? params[:friendly_name] : nil
+          @csr.body = params[:csr]
+          @csr.ssl_account_id = current_user.ssl_account.id
+
+          if !current_user.ssl_account.all_csrs.find_by_public_key_sha1(@csr.public_key_sha1).blank?
+            returnObj['status'] = 'CSR already exists on team' + current_user.ssl_account.ssl_slug + '.'
+          elsif @csr.save
+            returnObj['status'] = 'true'
+            returnObj['csr_ref'] = @csr.ref
+            returnObj['hash'] = @csr.public_key_sha1
+          else
+            returnObj['status'] = 'There was a problem adding this CSR to the CSR Manager.'
+          end
+        end
       else
-        returnObj['status'] = 'There was a problem adding this CSR to the CSR Manager.'
+        returnObj['status'] = 'no_user'
+        returnObj['url'] = new_user_session_url
       end
     else
       @csr = ManagedCsr.new
-      @csr.friendly_name = params[:friendly_name] && !params[:friendly_name].empty? ? params[:friendly_name] : nil
       @csr.body = params[:csr]
-      @csr.ssl_account_id = current_user.ssl_account.id
 
-      if !current_user.ssl_account.all_csrs.find_by_public_key_sha1(@csr.public_key_sha1).blank?
-        returnObj['status'] = 'CSR already exists on team' + current_user.ssl_account.ssl_slug + '.'
-      elsif @csr.save
-        returnObj['status'] = 'true'
-        returnObj['csr_ref'] = @csr.ref
-        returnObj['hash'] = @csr.public_key_sha1
-      else
-        returnObj['status'] = 'There was a problem adding this CSR to the CSR Manager.'
-      end
+      returnObj['status'] = 'true'
+      returnObj['hash'] = @csr.public_key_sha1
     end
 
     render :json => returnObj
@@ -139,5 +156,23 @@ class ManagedCsrsController < ApplicationController
     Csr.per_page = @per_page if Csr.per_page != @per_page
 
     @p = {page: (params[:page] || 1), per_page: @per_page}
+  end
+
+  def set_sign_hash_algorithms
+    @sign_alg = [
+        ['RSASSA-PKCS1-v1_5', 'RSASSA-PKCS1-v1_5'],
+        ['ECDSA', 'ECDSA'],
+        ['RSA-PSS', 'RSA-PSS']
+    ]
+
+    @rsa_key_size = [
+        ['2048', '2048'],
+        ['4096', '4096']
+    ]
+
+    @ec_key_size = [
+        ['256', '256'],
+        ['384', '384']
+    ]
   end
 end
