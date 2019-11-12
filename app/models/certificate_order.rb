db9c085c1140e4989c84460223fae5aa7e5658fa
codelux <leo@ssl.com>
2019-11-12 12:47:10 -0600

    fix CertificateName#unvalidated

--- app/models/certificate_order.rb
+++ app/models/certificate_order.rb
@@ -1213,6 +1213,10 @@ class CertificateOrder < ActiveRecord::Base
   end
 
   def apply_for_certificate(options={})
+    # set multiple_signed_certificates to true when manually requesting a new signed certificate which can result
+    # in several signed_certificates belonging to the same csr thus certificate_content
+    (return false if !certificate_content.signed_certificate.blank? or certificate_content.pending_issuance?) unless
+      options[:multiple_signed_certificates]
     if [Ca::CERTLOCK_CA,Ca::SSLCOM_CA,Ca::MANAGEMENT_CA].include?(options[:ca]) or certificate_content.ca or
         !options[:mapping].blank?
       if !certificate_content.infringement.empty? # possible trademark problems
