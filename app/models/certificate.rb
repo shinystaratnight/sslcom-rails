20545922c4581c94c99953b61f0555a8d258fc6d
codelux <leo@ssl.com>
2019-11-15 16:55:15 -0600

    fix for https://sslcom.airbrake.io/projects/128852/groups/2598240612766794871/notices/2598240642491055503

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -310,9 +310,9 @@ class Certificate < ActiveRecord::Base
   end
   memoize :items_by_duration
 
-  def pricing(certificate_order)
-    Rails.cache.fetch("#{cache_key}/#{certificate_order.certificate_content.cache_key if
-        certificate_order.certificate_content.try(:cache_key)}/pricing", expires_in: 1.hour) do
+  def pricing(certificate_order,certificate_content)
+    Rails.cache.fetch("#{cache_key}/#{certificate_order.try(:cache_key)}/#{certificate_content.domains}",
+                      expires_in: 1.hour) do
       ratio = certificate_order.signed_certificate ? certificate_order.duration_remaining : 1
       durations=[]
       result={}
@@ -335,7 +335,7 @@ class Certificate < ActiveRecord::Base
         num_durations.times do |i|
           licenses << (items_by_server_licenses[i].price*ratio).format
         end
-        result.merge!(licenses: licenses, domains: certificate_order.certificate_content.domains)
+        result.merge!(licenses: licenses, domains: certificate_content.domains)
       end
       result.merge!(product: product)
     end
