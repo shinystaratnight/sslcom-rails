306f96afa19f5fd2db51cbfdcb65f23d2af6b8d7
Antarr Byrd <antarr.byrd@gmail.com>
2019-12-09 09:06:19 -0600

    Move act_as_* libraries into ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
+require 'acts_as_billable/lib/active_merchant_default_gateway'
+require 'acts_as_billable/lib/acts_as_sellable'
+require 'acts_as_billable/lib/acts_as_seller'
+require 'acts_as_billable/lib/acts_as_billable'
+
+require 'acts_as_money/aggregations'
+require 'acts_as_money/acts_as_money'
+
+require 'acts_as_notable/lib/notable_methods'
+require 'acts_as_notable/lib/note_methods'
+
+require 'acts_as_publishable/lib/acts_as_publishable'
+
 class ApplicationRecord < ActiveRecord::Base
   include Swagger::Blocks
+  include CollectiveIdea::Acts::Billable
+  include CollectiveIdea::Acts::Sellable
+  include CollectiveIdea::Acts::Money
+  include Acts::As::Publishable
 
   self.abstract_class = true
 end
