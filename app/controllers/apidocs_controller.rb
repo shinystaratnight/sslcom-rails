5fee725e5f4501a8faad1bdc97c54cdd2f17d060
Antarr Byrd <antarr.byrd@gmail.com>
2019-12-11 16:01:01 -0600

    Move documentation copy to a seperate localized file

--- app/controllers/apidocs_controller.rb
+++ app/controllers/apidocs_controller.rb
@@ -37,13 +37,184 @@ class ApidocsController < ApplicationController
       key :in, :query
       key :description, 'The page number. Example: if per_page is set to 10, and page is 5, then records 51-60 will be returned.'
     end
+    parameter :product do
+      key :name, :product
+      key :in, :query
+      key :type, :number
+      key :format, :integer
+      key :required, true
+      key :description, I18n.t(:product_param_description, scope: :documentation)
+      key :example, 100
+    end
+    parameter :period do
+      key :name, :period
+      key :in, :query
+      key :type, :number
+      key :format, :integer
+      key :required, true
+      key :description, I18n.t(:period_param_description, scope: :documentation)
+    end
+    parameter :unique_value do
+      key :name, :unique_value
+      key :in, :query
+      key :type, :string
+      key :format, :byte
+      key :description, I18n.t(:unique_value_param_description, scope: :documentation)
+    end
+    parameter :csr do
+      key :name, :csr
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:csr_param_description, scope: :documentation)
+    end
+    parameter :server_software do
+      key :name, :server_software
+      key :in, :query
+      key :type, :integer
+      key :description, I18n.t(:server_software_description, scope: :documentation)
+    end
+    parameter :domains do
+      key :name, :domains
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:domains_param_description, scope: :documentation)
+    end
+    parameter :organization do
+      key :name, :organization
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:organization_param_description, scope: :documentation)
+      key :required, true
+    end
+    parameter :organization_unit do
+      key :name, :organization_unit
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:organization_unit_param_description, scope: :documentation)
+    end
+    parameter :post_office_box do
+      key :name, :post_office_box
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:post_office_box_param_description, scope: :documentation)
+    end
+    parameter :street_address_1 do
+      key :name, :street_address_1
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:street_address_1_param_description, scope: :documentation)
+      key :required, true
+    end
+    parameter :street_address_2 do
+      key :name, :street_address_2
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:street_address_2_param_description, scope: :documentation)
+    end
+    parameter :street_address_3 do
+      key :name, :street_address_3
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:street_address_3_param_description, scope: :documentation)
+    end
+    parameter :locality do
+      key :name, :locality
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:locality_param_description, scope: :documentation)
+    end
+    parameter :state_or_providence do
+      key :name, :state_or_providence
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:state_or_providence_param_description, scope: :documentation)
+      key :required, true
+    end
+    parameter :country do
+      key :name, :country
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:country_param_description, scope: :documentation)
+      key :required, true
+    end
+    parameter :duns_number do
+      key :name, :duns_number
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:duns_number_param_description, scope: :documentation)
+    end
+    parameter :company_number do
+      key :name, :company_number
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:company_number_params_description, scope: :documentation)
+    end
+    parameter :joi do
+      key :name, :joi
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:joi_param_description, scope: :documentation)
+    end
+    parameter :ca_certificate_id do
+      key :name, :ca_certificate_id
+      key :in, :query
+      key :description, I18n.t(:ca_certificate_id_param_description, scope: :documentation)
+    end
+    parameter :external_order_number do
+      key :name, :external_order_number
+      key :in, :query
+      key :description, I18n.t(:external_order_number_param_description, scope: :documentation)
+    end
+    parameter :hide_certificate_reference do
+      key :name, :hide_certificate_reference
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:hide_certificate_reference_description, scope: :documentation)
+    end
+    parameter :callback do
+      key :name, :callback
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:callback_param_description, scope: :documentation)
+    end
+    parameter :contacts do
+      key :name, :contacts
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:contacts_param_description, scope: :documentation)
+      key :required, true
+    end
+    parameter :app_req do
+      key :name, :app_req
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:app_req_param_description, scope: :documentation)
+    end
+    parameter :payment_method do
+      key :name, :payment_method
+      key :in, :query
+      key :type, :object
+      key :description, I18n.t(:payment_method_param_description, scope: :documentation)
+    end
+    parameter :response_type do
+      key :name, :response_type
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:payment_method_param_description, scope: :documentation)
+    end
+    parameter :response_encoding do
+      key :name, :response_encoding
+      key :in, :query
+      key :type, :string
+      key :description, I18n.t(:payment_method_param_description, scope: :documentation)
+    end
 
     tag do
       key :name, 'user'
       key :description, 'User Operations'
       externalDocs do
         key :description, 'Find more info here'
-        key :url, 'https://www.ssl.com/guide/ssl-coms-sws-api-introduction/'
+        key :url, 'https://www.ssl.com/guide/ssl-coms-sws-api-introduction#access'
       end
     end
     tag do
@@ -51,7 +222,7 @@ class ApidocsController < ApplicationController
       key :description, I18n.t(:certificate_tag_description, scope: :documentation)
       externalDocs do
         key :description, 'Find more info here'
-        key :url, 'https://www.ssl.com/guide/ssl-coms-sws-api-introduction/'
+        key :url, 'https://www.ssl.com/guide/ssl-coms-sws-api-introduction/#issue'
       end
     end
     key :host, 'sws.sslpki.local:3000'
@@ -62,13 +233,13 @@ class ApidocsController < ApplicationController
 
   # A list of all classes that have swagger_* declarations.
   SWAGGERED_CLASSES = [
+    Swagger::Blocks::SslCertificatesPath,
     Api::V1::ApiUserRequestsController,
     User,
     Swagger::Blocks::ErrorResponse,
     Swagger::Blocks::CredentialsResponse,
     Swagger::Blocks::CreateCertificateResponse,
-    Api::V1::ApiCertificateRequestsController,
-    Swagger::Blocks::SslCertificatesPath,
+    # Api::V1::ApiCertificateRequestsController,
     self
   ].freeze
 
--- app/paths/swagger/blocks/ssl_certificates_path.rb
+++ app/paths/swagger/blocks/ssl_certificates_path.rb
@@ -100,172 +100,31 @@ module Swagger
           security account_key: []
           security secret_key: []
 
-          parameter do
-            key :name, :product
-            key :in, :query
-            key :type, :number
-            key :format, :integer
-            key :required, true
-            key :description, I18n.t(:product_param_description, scope: :documentation)
-            key :example, 100
-          end
-          parameter do
-            key :name, :period
-            key :in, :query
-            key :type, :number
-            key :format, :integer
-            key :required, true
-            key :description, I18n.t(:period_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :unique_value
-            key :in, :query
-            key :type, :string
-            key :format, :byte
-            key :description, I18n.t(:unique_value_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :csr
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:csr_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :server_software
-            key :in, :query
-            key :type, :integer
-            key :description, I18n.t(:server_software_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :domains
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:domains_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :organization
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:organization_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :organization_unit
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:organization_unit_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :post_office_box
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:post_office_box_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :street_address_1
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:street_address_1_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :organization_unit
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:street_address_2_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :street_address_3
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:street_address_3_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :locality
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:locality_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :state_or_providence
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:state_or_providence_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :state_or_providence
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:state_or_providence_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :country
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:country_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :duns_number
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:duns_number_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :company_number
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:company_number_params_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :joi
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:joi_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :ca_certificate_id
-            key :in, :query
-            key :description, I18n.t(:ca_certificate_id_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :external_order_number
-            key :in, :query
-            key :description, I18n.t(:external_order_number_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :hide_certificate_reference
-            key :in, :query
-            key :type, :string
-            key :description, I18n.t(:hide_certificate_reference_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :callback
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:callback_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :contacts
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:contacts_param_description, scope: :documentation)
-            key :required, true
-          end
-          parameter do
-            key :name, :app_req
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:app_req_param_description, scope: :documentation)
-          end
-          parameter do
-            key :name, :payment_method
-            key :in, :query
-            key :type, :object
-            key :description, I18n.t(:payment_method_param_description, scope: :documentation)
-          end
+          parameter :product
+          parameter :period
+          parameter :unique_value
+          parameter :csr
+          parameter :server_software
+          parameter :domains
+          parameter :organization
+          parameter :organization_unit
+          parameter :post_office_box
+          parameter :street_address_1
+          parameter :street_address_2
+          parameter :street_address_3
+          parameter :locality
+          parameter :state_or_providence
+          parameter :country
+          parameter :duns_number
+          parameter :company_number
+          parameter :joi
+          parameter :ca_certificate_id
+          parameter :external_order_number
+          parameter :hide_certificate_reference
+          parameter :callback
+          parameter :contacts
+          parameter :app_req
+          parameter :payment_method
           response 201 do
             key :description, 'Certificate Created Response'
             schema do
@@ -278,6 +137,34 @@ module Swagger
               key :'$ref', :ErrorResponse
             end
           end
+        end
+      end
+
+      swagger_path '/certificate/ref' do
+        operation :get do
+          security account_key: []
+          security secret_key: []
+          key :summary, I18n.t(:certificate_order_summary, scope: :documentation)
+          key :description, I18n.t(:certificate_order_description, scope: :documentation)
+          key :operation, 'indexCertificates'
+          key :produces, %w[application/json]
+          key :consumes, %w[application/json]
+          key :tags, [
+            'certificate'
+          ]
+          parameter :ref
+          parameter :unique_value
+          parameter :csr do
+            key :required, true
+          end
+          parameter :server_software do
+            key :required, true
+          end
+          parameter :domains
+          parameter :organization do
+            key :required, true
+          end
+          parameter :organization_unit
         end
       end
     end
