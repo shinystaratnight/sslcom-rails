25640b857ea7fa9d301562142974826e9ca9bcd9
Antarr Byrd <antarr.byrd@gmail.com>
2019-12-19 11:07:51 -0600

    Style fixes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -15,9 +15,9 @@ module Api
       include Swagger::Blocks
 
       skip_before_action :verify_authenticity_token
-      before_filter :activate_authlogic
-      before_filter :set_default_request_format
-      after_filter  :set_access_control_headers
+      before_action :activate_authlogic
+      before_action :set_default_request_format
+      after_action  :set_access_control_headers
 
       TEST_SUBDOMAIN = 'sws-test'
       PER_PAGE_DEFAULT = 10
@@ -25,7 +25,7 @@ module Api
       respond_to :json
 
       rescue_from MultiJson::DecodeError do |exception|
-        render text: exception.to_s, status: 422
+        render text: exception.to_s, status: :unprocessable_entity
       end
 
       private
@@ -49,15 +49,15 @@ module Api
                else
                  { errors: @result.errors }
                end
-        render json: json, status: 200
+        render json: json, status: :ok
       end
 
       def render_200_status
-        render template: @template, status: 200
+        render template: @template, status: :ok
       end
 
       def render_400_status
-        render template: @template, status: 400
+        render template: @template, status: :bad_request
       end
 
       def render_500_error(e)
@@ -67,13 +67,13 @@ module Api
       end
 
       def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*' if Rails.env="development" # nginx handles this in production
+        headers['Access-Control-Allow-Origin'] = '*' if Rails.env = 'development' # nginx handles this in production
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
       end
 
-      def has_api_access?
+      def api_access?
         ak = params[:account_key]
         sk = params[:secret_key]
         return false if ak.blank? || sk.blank?
