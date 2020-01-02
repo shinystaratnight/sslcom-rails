32e4524453913b1aa0d5200b0fdd4f61f923d7d9
Antarr Byrd <antarr.byrd@gmail.com>
2020-01-02 11:05:32 -0600

    Remove Compass and Duplicate Mixins

--- config/compass.rb
+++ config/compass.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 # project_type = :rails
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "public/stylesheets/compiled"
-http_stylesheets_path = "/stylesheets"
-sass_dir = "app/asstes/stylesheets"
-images_dir = "public/images"
-http_images_path = "/images"
-javascripts_dir = "public/javascripts"
-http_javascripts_path = "/javascripts"
-sass_options = {line_numbers: true, style: :expanded, never_update: false}
-#sass_options = {debug_info: true, style: :expanded} #use when firesass works for firebug 1.6
-#sass_options = {style: :compressed} #for production use
+http_path = '/'
+css_dir = 'public/stylesheets/compiled'
+http_stylesheets_path = '/stylesheets'
+sass_dir = 'app/asstes/stylesheets'
+images_dir = 'public/images'
+http_images_path = '/images'
+javascripts_dir = 'public/javascripts'
+http_javascripts_path = '/javascripts'
+sass_options = { line_numbers: true, style: :expanded, never_update: false }
+# sass_options = {debug_info: true, style: :expanded} #use when firesass works for firebug 1.6
+# sass_options = {style: :compressed} #for production use
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
