abd7bed156ea97b9a611218afa9795c855af81d4
Antarr Byrd <antarr.byrd@gmail.com>
2020-01-06 10:14:53 -0600

    Add lol_dba gem and missing indexes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#   
+#
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 Ca.create(id: 1, ref: "0", friendly_name: "SSL.com Shadow", profile_name: "Management CA", algorithm: "rsa", description: "SSL.com Shadow", profile_type: "certificate")
