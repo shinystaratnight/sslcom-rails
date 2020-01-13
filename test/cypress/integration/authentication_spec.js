4bc7ccdf05e24673b3d61e440e2f06d99916ad4b
Antarr Byrd <antarr.byrd@gmail.com>
2020-01-13 13:39:33 -0600

    Update authentication_spec.js

--- test/cypress/integration/authentication_spec.js
+++ test/cypress/integration/authentication_spec.js
@@ -1,62 +1,75 @@
 // test/cypress/integrations/authentication_spec.js
 describe('User authentication spec', function () {
+  before(() => {
+    cy.appEval("User.destroy_all")
+  })
+
   beforeEach(() => {
     cy.visit('/logout')
   })
 
-  it('removes users from previous tests', function(){
-    cy.appEval("User.destroy_all")
-  })
-
   it('allows user to register and login', function () {
-    cy.contains('Create a new account').click()
+    // Visit Root Page
+    cy.visit('/');
 
+    // Visit Account Page
+    cy.contains("MY ACCOUNT")
+      .click()
+
+    // Go To Login Page
+    cy.contains('Create a new account')
+      .click()
+
+    // Create User Account
     cy.get('form').within(($form) => {
-      cy.get('input[name="user[login]"]').type('chef')
-      cy.get('input[name="user[email]"]').type('chef@test.ssl.com')
-      cy.get('input[name="user[password]"]').type('Testing_ssl+1')
-      cy.get('input[name="user[password_confirmation]"]').type('Testing_ssl+1')
+      cy.get('input[name="user[login]"]').type('cypress')
+      cy.get('input[name="user[email]"]').type('cypress@test.ssl.com')
+      cy.get('input[name="user[password]"]').type('Password123!')
+      cy.get('input[name="user[password_confirmation]"]').type('Password123!')
       cy.get('input[name="tos"]').click()
+      cy.root().submit()
     })
-    cy.get('#next_submit').click()
 
+    // New User Is Redirected To Dashboard
     cy.contains('SSL.com Customer Dashboard')
   })
 
   it('allows existing user to login and logout', function () {
     cy.appFactories([
       ['create', 'user', {
-        login: 'randy'
+        login: 'existing'
       }]
     ])
 
-    cy.visit('/');
-
-    cy.contains("MY ACCOUNT").click()
+    // Go To Login Page
+    cy.visit('/user_session/new')
 
     // Fill In And Submit Login Form
     cy.get('form').within(($form) => {
-      cy.get('input#user_session_login').type('randy')
-      cy.get('input#user_session_password').type('Testing_ssl+1')
+      cy.get('input[name="user_session[login]"]').type('existing')
+      cy.get('input[name="user_session[password]"]').type('Testing_ssl+1')
+      cy.root().submit()
     })
 
-    cy.get('#next_submit').click()
 
     // User Is Redirected To Dashboard
-    cy.get('#content').contains('SSL.com Customer Dashboard')
+    cy.get('#content')
+      .should('not.contain', 'Customer login')
+      .contains('SSL.com Customer Dashboard')
 
-    // // Logout User
-    // cy.contains('Logout').click()
+    // Logout User
+    cy.contains('Logout')
+      .click()
 
-    // // Redirected to Login Page
-    // cy.contains('Customer login')
+    // Redirected to Login Page
+    cy.contains('Customer login')
   })
 
   it('fails gracefully when attempting to reset password with nonexistent login', function () {
     cy.visit('/password_resets/new')
 
     cy.get('form').within(($form) => {
-      cy.get('input[name="login"]').type('skeeter')
+      cy.get('input[name="login"]').type('nonexistent')
     })
     cy.get('.password_resets_btn').click()
     cy.contains('No user was found with that login')
@@ -65,14 +78,14 @@ describe('User authentication spec', function () {
   it('allows existing user to reset password using login', function () {
     cy.appFactories([
       ['create', 'user', {
-        login: 'liane'
+        login: 'existing'
       }]
     ])
 
     cy.visit('/password_resets/new')
 
     cy.get('form').within(($form) => {
-      cy.get('input[name="login"]').type('liane')
+      cy.get('input[name="login"]').type('existing')
     })
     cy.get('.password_resets_btn').click()
     cy.contains('Customer login')
@@ -81,14 +94,14 @@ describe('User authentication spec', function () {
   it('allows existing user to reset password using email', function () {
     cy.appFactories([
       ['create', 'user', {
-        email: 'cartman@ssl.com'
+        email: 'existing@ssl.com'
       }]
     ])
 
     cy.visit('/password_resets/new')
 
     cy.get('form').within(($form) => {
-      cy.get('input[name="email"]').type('cartman@ssl.com')
+      cy.get('input[name="email"]').type('existing@ssl.com')
     })
     cy.get('.password_resets_btn').click()
     cy.contains('Customer login')
@@ -98,25 +111,20 @@ describe('User authentication spec', function () {
     cy.visit('/password_resets/new')
 
     cy.get('form').within(($form) => {
-      cy.get('input[name="email"]').type('token@ssl.com')
+      cy.get('input[name="email"]').type('nonexistent@ssl.com')
     })
     cy.get('.password_resets_btn').click()
     cy.contains('No user was found with that email')
   })
 
   it('requires Duo 2FA when logging in as super_user', function () {
-    cy.appFactories([
-      ['create', 'user', {
-        login: 'kenny'
-      }]
-    ])
-    cy.appEval("User.find_by(login: 'kenny').make_super_user")
+    cy.app('super_user')
 
     cy.visit('/user_session/new')
 
     // Fill In And Submit Login Form
     cy.get('form').within(($form) => {
-      cy.get('input[name="user_session[login]"]').type('kenny')
+      cy.get('input[name="user_session[login]"]').type('superuser1')
       cy.get('input[name="user_session[password]"]').type('Testing_ssl+1')
       cy.root().submit()
     })
@@ -125,27 +133,5 @@ describe('User authentication spec', function () {
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/user_session/duo')
     })
-  })
-
-  it('allows super_user to login as another user', function () {
-    cy.setCookie('skip_duo', 'true')
-    cy.appFactories([
-      ['create', 'user', {
-        login: 'timmy'
-      }],
-      ['create_list', 'user', 5]
-    ])
-    cy.appEval("User.find_by(login: 'timmy').make_super_user")
-    cy.visit('/user_session/new')
-
-    // Fill In And Submit Login Form
-    cy.get('form').within(($form) => {
-      cy.get('input[name="user_session[login]"]').type('timmy')
-      cy.get('input[name="user_session[password]"]').type('Testing_ssl+1')
-      cy.root().submit()
-    })
-
-    // cy.visit('/account')
-    cy.get('#content').contains('Admin Dashboard')
   })
 })
