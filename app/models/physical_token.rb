52da87e2d8cf5e83aede0c6c67ec8513ec22cbe0
soichuong <soi@ssl.com>
2020-02-01 00:26:58 +0700

    addressed found by code climate

--- app/models/physical_token.rb
+++ app/models/physical_token.rb
@@ -23,7 +23,7 @@
 
 class PhysicalToken < ApplicationRecord
   MAKE_AND_MODELS={Gemalto: %w(5100\ eToken), Yubico: %w(Yubikey\ FIPS\ 140-2)}
-  CARRIERS=%w(Not\ Yet\ Shipped FedEx UPS USPS DHL in-person)
+  CARRIERS = ['Not Yet Shipped', 'FedEx', 'UPS', 'USPS', 'DHL', 'in-person']
 
   belongs_to :certificate_order
   belongs_to :signed_certificate
