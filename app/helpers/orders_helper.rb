154ed2fa55ee5f7a4fa2d5c45aae20669e4f17d3
Leo Grove <leo@ssl.com>
2020-02-20 10:44:40 +0100

    Update orders_helper.rb
    changing affiliates link from www.ssl.com/affiliate to affiliates.ssl.com

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -221,7 +221,7 @@ module OrdersHelper
       if @order.ext_affiliate_name=="shareasale"
         "<img src=\"https://shareasale.com/sale.cfm?amount=#{@order.final_amount.to_s}&tracking=#{@order.reference_number}&transtype=sale&merchantID=#{@order.ext_affiliate_id}\" width=\"1\" height=\"1\">".html_safe
       else
-        "<img border=\"0\" src=\"https://#{Settings.community_domain}/affiliate/sale.php?profile=#{@order.ext_affiliate_id}&idev_saleamt=#{@order.final_amount.to_s}&idev_ordernum=#{@order.reference_number}#{"&coupon_code="+@order.discounts.last.ref unless @order.discounts.empty?}\" width=\"1\" height=\"1\">".html_safe
+        "<img border=\"0\" src=\"https://affiliates.ssl.com/sale.php?profile=#{@order.ext_affiliate_id}&idev_saleamt=#{@order.final_amount.to_s}&idev_ordernum=#{@order.reference_number}#{"&coupon_code="+@order.discounts.last.ref unless @order.discounts.empty?}\" width=\"1\" height=\"1\">".html_safe
       end if @order.amount.cents > 0
     end
   end
