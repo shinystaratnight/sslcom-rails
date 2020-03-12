6201936cb10eef1470aaf8c324c85d67fd2b5a3e
codelux <leo@ssl.com>
2020-03-12 10:23:23 -0500

    add Debian weak key lists

--- db/migrate/20200311221607_seed_weak_keys.rb
+++ db/migrate/20200311221607_seed_weak_keys.rb
@@ -1,15 +1,15 @@
 class SeedWeakKeys < ActiveRecord::Migration
   def up
     wk=[]
-    File.open("lib/weak_keys/blacklist.RSA-1024", "r").each_line do |line|
+    File.foreach("lib/weak_keys/blacklist.RSA-1024") do |line|
       wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 1024)
     end
     WeakKey.import wk
-    File.open("lib/weak_keys/blacklist.RSA-2048", "r").each_line do |line|
+    File.foreach("lib/weak_keys/blacklist.RSA-2048") do |line|
       wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 2048)
     end
     WeakKey.import wk
-    File.open("lib/weak_keys/blacklist.RSA-4096", "r").each_line do |line|
+    File.foreach("lib/weak_keys/blacklist.RSA-4096") do |line|
       wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 4096)
     end
     WeakKey.import wk
