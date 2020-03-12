30daa31502ed82bda1a3da54fe5c5d60c73ad85f
Leo Grove <leo@ssl.com>
2020-03-12 16:57:32 -0500

    Update 20200311215628_create_weak_keys.rb

--- db/migrate/20200311215628_create_weak_keys.rb
+++ db/migrate/20200311215628_create_weak_keys.rb
@@ -1,7 +1,7 @@
 class CreateWeakKeys < ActiveRecord::Migration
   def change
     create_table :weak_keys do |t|
-      t.string    :sha1_hash
+      t.string    :sha1_hash, length: 20
       t.string    :algorithm
       t.integer   :size
     end
