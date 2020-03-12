9c259102bac2c754d3e79d74e111d85ec0fe759a
codelux <leo@ssl.com>
2020-03-12 12:10:06 -0500

    remove newline for weak keys lists during import

--- db/migrate/20200311221607_seed_weak_keys.rb
+++ db/migrate/20200311221607_seed_weak_keys.rb
@@ -2,15 +2,15 @@ class SeedWeakKeys < ActiveRecord::Migration
   def up
     wk=[]
     File.foreach("lib/weak_keys/blacklist.RSA-1024") do |line|
-      wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 1024)
+      wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 1024)
     end
     WeakKey.import wk
     File.foreach("lib/weak_keys/blacklist.RSA-2048") do |line|
-      wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 2048)
+      wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 2048)
     end
     WeakKey.import wk
     File.foreach("lib/weak_keys/blacklist.RSA-4096") do |line|
-      wk << WeakKey.new(sha1_hash: line, algorithm: "RSA", size: 4096)
+      wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 4096)
     end
     WeakKey.import wk
   end
