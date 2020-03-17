6386d48879323a01722a3a3cad27b52a44d54803
Antarr Byrd <antarr.byrd@gmail.com>
2020-03-17 07:00:31 -0500

    Update factories

--- db/migrate/20200311221607_seed_weak_keys.rb
+++ db/migrate/20200311221607_seed_weak_keys.rb
@@ -4,14 +4,17 @@ class SeedWeakKeys < ActiveRecord::Migration
       wk=[]
       File.foreach("lib/weak_keys/blacklist.RSA-1024") do |line|
         wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 1024)
+        break unless Rails.env.production? # record should be good enough for development
       end
       WeakKey.import wk
       File.foreach("lib/weak_keys/blacklist.RSA-2048") do |line|
         wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 2048)
+        break unless Rails.env.production?
       end
       WeakKey.import wk
       File.foreach("lib/weak_keys/blacklist.RSA-4096") do |line|
         wk << WeakKey.new(sha1_hash: line.chomp, algorithm: "RSA", size: 4096)
+        break unless Rails.env.production?
       end
       WeakKey.import wk
     end
