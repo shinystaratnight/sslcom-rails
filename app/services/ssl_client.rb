659fa9c4df49b0805711c3275c792dceee2464b5
Daniel Rubio <drubio1989@gmail.com>
2020-03-23 16:25:18 -0500

    Add timeout code: Do not merge yet

--- app/services/ssl_client.rb
+++ app/services/ssl_client.rb
@@ -7,25 +7,25 @@ class SslClient
   end
 
   def retrieve_x509_cert
-    cert_store = OpenSSL::X509::Store.new
-    cert_store.set_default_paths
-    context = OpenSSL::SSL::SSLContext.new
-    context.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    context.cert_store = cert_store
-    tcp_client = TCPSocket.new(url, port)
-    ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client, context
-    ssl_client.hostname = url
-    ssl_client.sync_close = true
-    ssl_client.connect
-    certificate = ssl_client.peer_cert
-    tcp_client.close
-    certificate
-  rescue
-    nil
+    Timeout::timeout(3, nil, 'time out') do # BUG. Never do this.
+      cert_store = OpenSSL::X509::Store.new
+      cert_store.set_default_paths
+      context = OpenSSL::SSL::SSLContext.new
+      context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      context.cert_store = cert_store
+      tcp_client = TCPSocket.new(url, port)
+      ssl_client = OpenSSL::SSL::SSLSocket.new tcp_client, context
+      ssl_client.hostname = url
+      ssl_client.sync_close = true
+      ssl_client.connect
+      certificate = ssl_client.peer_cert
+      tcp_client.close
+      certificate
+    end
   end
 
   def verify_result
-    result = %x"echo QUIT | openssl s_client -CApath /etc/ssl/certs/ -servername #{url} -verify_hostname #{url} -connect #{url}:#{port}"
+    result = %x"echo QUIT | timeout 3 openssl s_client -CApath /etc/ssl/certs/ -servername #{url} -verify_hostname #{url} -connect #{url}:#{port}"
     result.match(/Verify return code: (.*)/)[1].scan(/\(([^\)]+)\)/).flatten[0].downcase
   rescue
     nil
