12d71a9c26ae6f6652a868b395fbcdeff5d4926b
Antarr Byrd <antarr.byrd@gmail.com>
2020-03-25 16:02:59 -0500

    revert initializers

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 # You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
-Rails.backtrace_cleaner.add_silencer { |line| line =~ %r{/var/lib/gems/} }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /\/var\/lib\/gems\// }
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -1,5 +1,5 @@
 # RailsConfig.load_and_set_settings("/config/client/certassure")
-if DEPLOYMENT_CLIENT =~ /certassure/i && Rails.root.to_s =~ /Development/
-  RailsConfig.load_and_set_settings(Rails.root.join('config', 'client', 'certassure', 'settings.yml'))
-  Settings.reload_from_files(Rails.root.join('config', 'client', 'certassure', 'settings.yml'))
+if DEPLOYMENT_CLIENT=~/certassure/i && Rails.root.to_s=~/Development/
+  RailsConfig.load_and_set_settings(Rails.root.join("config","client","certassure", "settings.yml"))
+  Settings.reload_from_files(Rails.root.join("config","client","certassure", "settings.yml"))
 end
