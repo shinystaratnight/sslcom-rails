84944cd4b0c081557d6f87a3b6002ffc712bac19
Antarr Byrd <antarr.byrd@gmail.com>
2020-03-26 16:33:12 -0500

    Update contacts_spec.rb

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe 'Contacts', type: :feature do
 
   it 'can add an administrative contact', js: true do
     Authorization::Maintenance.without_access_control do
+      user.create_ssl_account if user.ssl_account.nil?
       user.ssl_account.generate_funded_account
       user.ssl_account.funded_account.update(cents: 100_000)
     end
