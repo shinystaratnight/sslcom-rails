5c6b34fb895b759c3861210d654ee03df166c95f
Daniel Rubio <drubio1989@gmail.com>
2020-03-30 15:01:42 -0500

    Change app deployment start up and initialization to production

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/sslcom-rails
+sudo chown ubuntu:ubuntu -R .
+cp ../*.yml /home/ubuntu/sslcom-rails/config/.
 RAILS_ENV=production bundle install
+RAILS_ENV=production bundle exec rake assets:precompile
--- script/prepare_application.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/sslcom-rails
-cp ../*.yml /home/ubuntu/sslcom-rails/config/.
-sudo chown ubuntu:ubuntu -R .
