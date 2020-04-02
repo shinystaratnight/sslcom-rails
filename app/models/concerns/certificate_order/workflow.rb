cc3e925bd392dfffef620abe1f7b7c9406d50aa9
Antarr Byrd <antarr.byrd@gmail.com>
2020-04-02 14:19:41 -0500

    Update workflow.rb

--- app/models/concerns/certificate_order/workflow.rb
+++ app/models/concerns/certificate_order/workflow.rb
@@ -15,28 +15,28 @@ module Concerns
             event :reject, transitions_to: :rejected
             event :cancel, transitions_to: :canceled
           end
-      
+
           state :paid do
             event :cancel, transitions_to: :canceled
             event :reject, transitions_to: :rejected
             event :refund, transitions_to: :refunded
             event :charge_back, transitions_to: :charged_back
-            event :start_over, transitions_to: :paid do |complete=false|
-              if self.certificate_contents.count > 1
-                cc = self.certificate_contents.last
+            event :start_over, transitions_to: :paid do |complete = false|
+              if certificate_contents.count > 1
+                cc = certificate_contents.last
                 cc.preserve_certificate_contacts
                 cc.delete
               else
-                duration = self.certificate_content.duration
-                temp_cc = self.certificate_contents.create(duration: duration)
+                duration = certificate_content.duration
+                temp_cc = certificate_contents.create(duration: duration)
                 # Do not delete the last one
-                (self.certificate_contents - [temp_cc]).each do |cc|
-                  cc.delete if ((cc.csr or cc.csr.try(:signed_certificate)) || complete)
+                (certificate_contents - [temp_cc]).each do |cc|
+                  cc.delete if (cc.csr || cc.csr.try(:signed_certificate)) || complete
                 end
               end
             end
           end
-      
+
           state :canceled do
             event :uncancel, transitions_to: :paid
             event :unrefund, transitions_to: :canceled
@@ -45,16 +45,16 @@ module Concerns
             event :charge_back, transitions_to: :charged_back
             event :cancel, transitions_to: :canceled
           end
-      
-          state :refunded do #only refund a canceled order
+
+          state :refunded do # only refund a canceled order
             event :unrefund, transitions_to: :paid
             event :reject, transitions_to: :rejected
             event :charge_back, transitions_to: :charged_back
           end
-      
+
           state :charged_back
-      
-          state :rejected do #only refund a canceled order
+
+          state :rejected do # only refund a canceled order
             event :cancel, transitions_to: :canceled
             event :unreject, transitions_to: :paid
             event :refund, transitions_to: :refunded
