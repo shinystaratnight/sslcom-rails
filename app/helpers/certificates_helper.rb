7f8e945f1f49b38f7f36acb45f7602299928d568
Antarr Byrd <antarr.byrd@gmail.com>
2020-04-03 11:10:28 -0500

    Update certificates_helper.rb

--- app/helpers/certificates_helper.rb
+++ app/helpers/certificates_helper.rb
@@ -13,6 +13,8 @@ module CertificatesHelper
   def new_certificate_params
     if @certificate_order.is_unused_credit?
       [@certificate_order, {:url=>:update_csr_certificate_order}]
+#    elsif @certificate.is_free?
+#      [@certificate_order, {url: :create_free_ssl}]
     elsif current_user && current_user.ssl_account.is_registered_reseller?
       @certificate_order
     else
@@ -22,5 +24,36 @@ module CertificatesHelper
 
   def buy_or_get
     @certificate.is_free? ? "Get" : "Buy"
+  end
+
+  def pricing(certificate)
+    last_duration_pricing certificate
+  end
+
+  def last_duration_pricing(certificate)
+    years = certificate.last_duration.value.to_i/365
+    years = 1 unless years > 0
+    factor =  certificate.is_ucc? ? 1 : 1
+    p = lambda do |certificate|
+            if certificate.is_ucc?
+              certificate.first_domains_tiers.last.price * 3
+            else
+              certificate.last_duration.price
+            end
+    end
+    price = p.call(certificate)
+    orig_price = p.call(certificate.untiered)
+    actual = (price/years).format
+    orig = (certificate.tiered? ?
+     (orig_price/years).format : nil) unless certificate.is_dv?
+    render :partial=>'pricing', :locals=>{:actual=>actual, :orig=>orig}
+  end
+
+  def first_duration_pricing
+    actual = certificate.first_duration.price.format
+    orig = (certificate.tiered? ?
+     certificate.untiered.first_duration.price.format : nil) unless
+    certificate.is_dv?
+    render :partial=>'pricing', :locals=>{:actual=>actual, :orig=>orig}
   end
 end
