27c5432beb1f6de770758b1ef69cab956f5c6591
antarr <974295+antarr@users.noreply.github.com>
2020-05-29 12:15:19 -0500

    JQuery and Select2 missing for inline scripts (#1305)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.sass, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[ ssl_com.css certassure.css common.sass print.css
+Rails.application.config.assets.precompile += %w[ ssl_com.css certassure.css common.sass print.css select2.js
                                                   ie.css ie8.css site_report.css buy_now.css ssl_seal.css form.authy.css flags.authy.css common.js shared.js jstz.min.js
-                                                  promise.min.js webcrypto-liner.shim.js asmcrypto.min.js elliptic.min.js bundle.umd.min.js bundle.pkcs.umd.min.js
+                                                  promise.min.js webcrypto-liner.shim.js asmcrypto.min.js elliptic.min.js bundle.umd.min.js bundle.pkcs.umd.min.js jquery.js
                                                   psl.min.js form.authy.js u2f-api.js Duo-Web-v2.js jquery.timepicker.min.js invoice.css bootstrap_style.sass bootstrap_fix.sass
                                                   jquery.timepicker.min.css delayed/web/application.css font_awesome5.js jquery.prettyLoader.js jquery.prettyPopin.js jquery.prettyPhoto.js]
