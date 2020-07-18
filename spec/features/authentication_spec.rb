fa5a6cdfd352ac66ac9ba7ed185ec3f1a44b009f
Rich Downie <rich@blackboxd.com>
2020-07-18 09:56:47 -0400

    refactor js tag

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Authentications', type: :feature do
+RSpec.describe 'Authentications', type: :feature, js: true  do
   let!(:user) { create(:user, :owner) }
   let!(:super_user) {create(:user, :super_user)}
   let!(:login_page) {LoginPage.new}
@@ -12,7 +12,7 @@ RSpec.describe 'Authentications', type: :feature do
     User.any_instance.stubs(:authenticated_avatar_url).returns('https://github.blog/wp-content/uploads/2012/03/codercat.jpg?fit=896%2C896')
   end
 
-  scenario 'logins in user who registers automatically', authentication: true, js: true do
+  scenario 'logins in user who registers automatically', authentication: true do
     registering = attributes_for(:user, :owner)
     login_page.load
     login_page.create_a_new_account_link.click
@@ -25,48 +25,48 @@ RSpec.describe 'Authentications', type: :feature do
     expect(page).to have_content('SSL.com Customer Dashboard')
   end
 
-  scenario 'allows existing user to login and logout', authentication: true, js: true do
+  scenario 'allows existing user to login and logout', authentication: true do
     login_page.load
     login_page.login_with(user)
     header.logout.click
     expect(page).to have_content('Successfully logged out.')
   end
 
-  scenario 'fails gracefully when attempting to reset password with nonexistent login', authentication: true, js: true do
+  scenario 'fails gracefully when attempting to reset password with nonexistent login', authentication: true do
     reset_password_page.load
     reset_password_page.login.set 'nonexistent'
     reset_password_page.submit.click
     expect(page).to have_content 'No user was found with that login'
   end
 
-  scenario 'allows existing user to reset password using login', authentication: true, js: true do
+  scenario 'allows existing user to reset password using login', authentication: true do
     reset_password_page.load
     reset_password_page.login.set user.login
     reset_password_page.submit.click
     expect(page).to have_content 'Customer login'
   end
 
-  scenario 'allows existing user to reset password using email', authentication: true, js: true do
+  scenario 'allows existing user to reset password using email', authentication: true do
     reset_password_page.load
     reset_password_page.email.set user.email
     reset_password_page.submit.click
     expect(page).to have_content 'Customer login'
   end
 
-  scenario 'fails gracefully when attempting to reset a password with nonexistent email', authentication: true, js: true do
+  scenario 'fails gracefully when attempting to reset a password with nonexistent email', authentication: true do
     reset_password_page.load
     reset_password_page.email.set 'nonexistent@ssl.com'
     reset_password_page.submit.click
     expect(page).to have_content 'No user was found with that email'
   end
 
-  scenario 'requires Duo 2FA when logging in as super_user', authentication: true,  js: true do
+  scenario 'requires Duo 2FA when logging in as super_user', authentication: true do
     login_page.load
     login_page.login_with(super_user)
     expect(page).to have_content 'Duo 2-factor authentication setup'
   end
 
-  xit 'disallows sysadmin to view "Send to SSL.com CA" page', authentication: true,  js: true do
+  xit 'disallows sysadmin to view "Send to SSL.com CA" page', authentication: true do
     other = create(:user)
     as_user(create(:user, :sys_admin)) do
       visit certificate_order_path(ref: "co-10000")
@@ -74,7 +74,7 @@ RSpec.describe 'Authentications', type: :feature do
     end
   end
 
-  scenario 'superuser 30 min session logout', authentication: true,  js: true do
+  scenario 'superuser 30 min session logout', authentication: true do
     login_page.load
     login_page.login_with(super_user)
     Timecop.travel(Time.current + 30.minutes)
@@ -83,7 +83,7 @@ RSpec.describe 'Authentications', type: :feature do
   end
 
   context 'when user visited cart' do
-    xit 'redirect to cart after login', authentication: true, js: true do
+    xit 'redirect to cart after login', authentication: true do
       # Cart checkout
       visit show_cart_orders_path
       find('a#add_items_img').click
