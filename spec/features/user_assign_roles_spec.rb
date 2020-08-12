99f81b1a0577237ba3164a5dd1253363bcf50481
Rich Downie <rich@blackboxd.com>
2020-08-11 21:53:59 -0400

    sp-633 User with super_user role can assign roles for and user without super_user role (#1508)
    * sp-633 adding spec
    
    * sp-633 updates for cc
    
    * sp-633 fix flake

--- spec/features/user_assign_roles_spec.rb
+++ spec/features/user_assign_roles_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User Assign Roles', type: :feature, js: true do
   let!(:user_owner) { create(:user, :owner) }
   let!(:user_sysadmin) { create(:user, :sys_admin) }
+  let!(:user_super_user) { create(:user, :super_user) }
   let!(:login_page) {LoginPage.new}
   let!(:header) {Header.new}
   let!(:users_page) {UsersPage.new}
@@ -28,14 +29,18 @@ RSpec.describe 'User Assign Roles', type: :feature, js: true do
 
   it 'User with super_user role can assign roles for and user without super_user role' do
     login_page.load
-    login_page.login_with(user_sysadmin)
-    header.wait_until_logout_visible(wait: 5)
+    login_page.login_with(user_super_user)
+    expect(page).to have_content('MY ACCOUNT')
     users_page.load
     search.wait_until_search_field_visible(wait: 5)
     search.search_field.set user_owner.email
     users_page.search_button.click
     all('tr').last.click
-    click_link "login as #{user_owner.login}"
-    expect(page).to have_content('Successfully logged in.')
+    users_page.owner_link.click
+    edit_user_roles_page.super_user_checkbox.click
+    edit_user_roles_page.select_ssl_account
+    first("li[class='select2-results__option']").click
+    edit_user_roles_page.submit_button.click
+    expect(page).to have_content("#{user_owner.email} roles have been updated for teams:")
   end
 end
